2471f4ee298303eaf7a80b22044ba924e58b7ef0
Kai09523471 <ns25190hs@isk.ed.jp>
2026-03-09 09:08:58 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+let textSpeed = 40
+let currentText = ""
+let textIndex = 0
+let typing = false
+
+function showText(){
+
+currentText = story[storyIndex]
+textIndex = 0
+text.textContent = ""
+typing = true
+
+typeText()
+
+}
+
+function typeText(){
+
+if(textIndex < currentText.length){
+
+text.textContent += currentText[textIndex]
+textIndex++
+
+setTimeout(typeText, textSpeed)
+
+}else{
+
+typing = false
+
+}
+
+}
+
+
+if(state==="story"){
+
+if(e.key==="Enter"){
+
+if(typing){
+text.textContent = currentText
+typing = false
+return
+}
+
+storyIndex++
+
+if(storyIndex < story.length){
+showText()
+}
+
+}
